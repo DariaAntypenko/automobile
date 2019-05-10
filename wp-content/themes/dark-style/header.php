0142243d30a7e332c015f4bca44bb4070692ec80
Антипенко Дарья <d.antipenko@ds.local>
2019-05-10 17:53:48 +0300

    base

---
+++ wp-content/themes/dark-style/header.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">каталог</a></li>
+        <li><a href="#">документы</a></li>
+        <li><a href="#">растаможка</a></li>
+        <li><a href="#">контакты</a></li>
+      </ul>
+    </div>
+</nav>
